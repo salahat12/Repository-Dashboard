9975630a52fb49b54027b091cabab474b6f87e6f
sama alaa salahat <samasalahat66@gmail.com>
2026-08-18 12:15:20 +0300

    Rename issues summary model

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from models.issue_item import IssueItem
-from models.issues_summary import IssuesSummary
+from models.summary import IssuesSummary
 from models.repo_info import RepoInfo
 
 __all__ = ["RepoInfo", "IssueItem", "IssuesSummary"]
